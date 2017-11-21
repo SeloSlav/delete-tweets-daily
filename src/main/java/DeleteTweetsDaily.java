a7cce5b5842f833c2384e6062c2ed9491d00efb9
santafebound <martin.erlic@gmail.com>
2017-11-21 14:20:35 -0800

    beep for an hour test

---
+++ src/main/java/DeleteTweetsDaily.java
@@ -0,0 +1,10 @@
+public class DeleteTweetsDaily {
+
+    public static void main(String[] args) {
+
+        BeeperControl beeperControl = new BeeperControl();
+        beeperControl.beepForAnHour();
+
+    }
+
+}
